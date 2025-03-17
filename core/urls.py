3a3af10875567c9c4f270d3a80169cc35095cbf6
jruizndev <jorsqn@gmail.com>
2025-03-17 18:34:52 +0100

    feat(templates): add home page template with Tailwind components

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__reload__/', include('django_browser_reload.urls')),
+    path('', include('apps.dashboard.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
 ]
