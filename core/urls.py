c7baa385300c7a37d9b7d053a14330360230bdf7
jruizndev <jorsqn@gmail.com>
2025-03-20 08:39:28 +0100

    fix(analyzer): create analyzer template and fix urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
     path('', include('apps.scraper.urls')),
+    path('', include('apps.analyzer.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
 ]
