55f926ad0f1bd1542d3f7c79256389871d893370
jruizndev <jorsqn@gmail.com>
2025-03-21 15:04:50 +0100

    style(navbar): adjust design, order, and add new elements following digital newspaper style

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'Europe/Madrid'
