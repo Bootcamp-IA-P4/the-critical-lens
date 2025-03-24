81c4aaf7cae2540cc71fd6234bf77db2fa9b4f34
jruizndev <jorsqn@gmail.com>
2025-03-24 17:28:15 +0100

    Fix: Add amdin.py to read the db content on django-administrator

--- apps/scraper/admin.py
+++ apps/scraper/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import VerificationCategory, FactCheckArticle
 
-# Register your models here.
+@admin.register(VerificationCategory)
+class VerificationCategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the VerificationCategory model.
+    """
+    list_display = ('name', 'description')  
+    search_fields = ('name', 'description')
+    list_filter = ('name',)
+    readonly_fields = ('name', 'description')  
+
+@admin.register(FactCheckArticle)
+class FactCheckArticleAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the FactCheckArticle model.
+    """
+    list_display = ('title', 'verification_text', 'claim_source', 'publish_date')
+    list_filter = ('verification_category',)  
+    search_fields = ('title', 'claim', 'claim_source', 'content', 'tags')
+    readonly_fields = ('title', 'url', 'publish_date', 'author', 'verification_category', 
+                       'claim', 'claim_source', 'content', 'tags', 'scraped_at')
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'url', 'publish_date', 'author')
+        }),
+        ('Verification', {
+            'fields': ('verification_category', 'claim', 'claim_source')
+        }),
+        ('Content', {
+            'fields': ('content', 'tags')
+        }),
+        ('Internal Control', {
+            'fields': ('scraped_at',),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def verification_text(self, obj):
+        """Displays the verification category name as plain text"""
+        return obj.verification_category.name if obj.verification_category else "-"
+    verification_text.short_description = 'Verification'
